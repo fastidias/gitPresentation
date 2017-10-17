82933ac2113e4704cb979393dc30f12f8d5283be
Alexander Weiss <fastidias@googlemail.com>
2017-10-17 13:36:55 +0200

    adding images and finalizing content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke');
+var progress = require('bespoke-progress');
 var classes = require('bespoke-classes');
 var nav = require('bespoke-nav');
 var scale = require('bespoke-scale');
@@ -18,5 +19,6 @@ bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   hash(),
   prism(),
   multimedia(),
-  extern(bespoke)
+  extern(bespoke),
+  progress()
 ]);
