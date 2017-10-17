171d4973d432afbc5c00740259a3b05b1650f5d3
Alexander Weiss <fastidias@googlemail.com>
2017-10-17 16:49:50 +0200

    added progress bar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke');
+var progress = require('bespoke-progress');
 var classes = require('bespoke-classes');
 var nav = require('bespoke-nav');
 var scale = require('bespoke-scale');
@@ -12,11 +13,13 @@ var extern = require('bespoke-extern');
 // Bespoke.js
 bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   classes(),
+  progress(),
   nav(),
   scale(),
   bullets('.build, .build-items > *:not(.build-items)'),
   hash(),
   prism(),
   multimedia(),
-  extern(bespoke),
+  extern(bespoke)
 ]);
+
